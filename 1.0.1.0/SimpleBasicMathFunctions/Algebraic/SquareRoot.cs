c1c7c96d8a25d8e3b5fdbf3a21b8187571a2fbaf
Artur B <arturzubert@web.de>
2019-03-13 08:31:28 +0100

    Add files via upload

---
+++ 1.0.1.0/SimpleBasicMathFunctions/Algebraic/SquareRoot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MathFunctions.Algebraic
+{
+    public class SquareRoot
+    {
+        /// <summary>
+        /// Calculate Square root of double
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <returns></returns>
+        public static double Calculate(double num1)
+        {
+            return Math.Sqrt(num1);
+        }
+
+        /// <summary>
+        /// Calculate SquareRoot with repeat
+        /// </summary>
+        /// <param name="num1">Number under SquareRoot</param>
+        /// <param name="num2">Times it should be repeated</param>
+        /// <returns></returns>
+        public static double Calculate(double num1, int num2)
+        {
+            for (int i = 0; i == num2; i++)
+            {
+                num1 = Math.Sqrt(num1);
+            }
+
+            return num1;
+        }
+    }
+}
---
+++ 1.0.1.0/SimpleBasicMathFunctions/Basic/Add.cs
@@ -0,0 +1,220 @@
+﻿namespace MathFunctions.Basic
+{
+    /// <summary>
+    /// Class for the Addition function. (Basic)
+    /// Exist since V1.0.0.0
+    /// Last Change: V1.0.1.0 => Added up to 5 Values
+    /// </summary>
+    public class Add
+    {
+
+        #region Addition with Doubles
+        /// <summary>
+        /// Addition for Double type
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        public static double CalculateDouble(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        /// <summary>
+        /// Addition for Double type with 3 Values
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <param name="num3"></param>
+        /// <returns></returns>
+        public static double CalculateDouble(double num1, double num2, double num3)
+        {
+            return num1 + num2 + num3;
+        }
+
+        /// <summary>
+        /// Addition for Double type with 4 Values
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <param name="num3"></param>
+        /// <param name="num4"></param>
+        /// <returns></returns>
+        public static double CalculateDouble(double num1, double num2, double num3, double num4)
+        {
+            return num1 + num2 + num3 + num4;
+        }
+
+        /// <summary>
+        /// Addition for Double type with 5 Values
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <param name="num3"></param>
+        /// <param name="num4"></param>
+        /// <param name="num5"></param>
+        /// <returns></returns>
+        public static double CalculateDouble(double num1, double num2, double num3, double num4, double num5)
+        {
+            return num1 + num2 + num3 + num4 + num5;
+        }
+        #endregion
+
+        #region Addition with Integer(int16)
+
+        /// <summary>
+        /// Addition for Int type (Int16)
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        public static int CalculateInt(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        /// <summary>
+        /// Addition for Int type (Int16) with 3 Values
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <param name="num3"></param>
+        /// <returns></returns>
+        public static int CalculateInt(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+
+        /// <summary>
+        /// Addition for Int type (Int16) with 4 Values
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <param name="num3"></param>
+        /// <param name="num4"></param>
+        /// <returns></returns>
+        public static int CalculateInt(int num1, int num2, int num3, int num4)
+        {
+            return num1 + num2 + num3 + num4;
+        }
+
+        /// <summary>
+        /// Addition for Int type (Int16) with 5 Values
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <param name="num3"></param>
+        /// <param name="num4"></param>
+        /// <param name="num5"></param>
+        /// <returns></returns>
+        public static int CalculateInt(int num1, int num2, int num3, int num4, int num5)
+        {
+            return num1 + num2 + num3 + num5;
+        }
+        #endregion
+
+        #region Addition with Float
+        /// <summary>
+        /// Addition for Float type
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        public static float CalculateFloat(float num1, float num2)
+        {
+            return num1 + num2;
+        }
+
+        /// <summary>
+        /// Addition for Float type with 3 Values
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <param name="num3"></param>
+        /// <returns></returns>
+        public static float CalculateFloat(float num1, float num2, float num3)
+        {
+            return num1 + num2 + num3;
+        }
+
+        /// <summary>
+        /// Addition for Float type with 4 Values
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <param name="num3"></param>
+        /// <param name="num4"></param>
+        /// <returns></returns>
+        public static float CalculateFloat(float num1, float num2, float num3, float num4)
+        {
+            return num1 + num2 + num3 + num4;
+        }
+
+        /// <summary>
+        /// Addition for Float type with 5 Values
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <param name="num3"></param>
+        /// <param name="num4"></param>
+        /// <param name="num5"></param>
+        /// <returns></returns>
+        public static float CalculateFloat(float num1, float num2, float num3, float num4, float num5)
+        {
+            return num1 + num2 + num3 + num4 + num5;
+        }
+        #endregion
+
+        #region Addition with Long(Int64)
+        /// <summary>
+        /// Addition for Long type (Int64)
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        public static long CalculateLong(long num1, long num2)
+        {
+            return num1 + num2;
+        }
+
+        /// <summary>
+        /// Addition for Long type (Int64) with 3 Values
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <param name="num3"></param>
+        /// <returns></returns>
+        public static long CalculateLong(long num1, long num2, long num3)
+        {
+            return num1 + num2 + num3;
+        }
+
+        /// <summary>
+        /// Addition for Long type (Int64) with 4 Values
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <param name="num3"></param>
+        /// <param name="num4"></param>
+        /// <returns></returns>
+        public static long CalculateLong(long num1, long num2, long num3, long num4)
+        {
+            return num1 + num2 + num3 + num4;
+        }
+
+        /// <summary>
+        /// Addition for Long type (Int64) with 5 Values
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <param name="num3"></param>
+        /// <param name="num4"></param>
+        /// <param name="num5"></param>
+        /// <returns></returns>
+        public static long CalculateLong(long num1, long num2, long num3, long num4, long num5)
+        {
+            return num1 + num2 + num3 + num4 + num5;
+        } 
+        #endregion
+    }
+}
